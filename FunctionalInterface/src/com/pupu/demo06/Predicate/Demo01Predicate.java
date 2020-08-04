287df46d109b132eaddf44ab8bb109b9a089e7c8
puli <123@123.com>
2020-08-05 07:30:37 +0800

    master 函数式编程

---
+++ FunctionalInterface/src/com/pupu/demo06/Predicate/Demo01Predicate.java
@@ -0,0 +1,19 @@
+package com.pupu.demo06.Predicate;
+
+import java.util.function.Predicate;
+
+/**
+ * @author : lipu
+ * @since : 2020-08-04 22:48
+ */
+public class Demo01Predicate {
+
+    private static void method(String name,Predicate<String> predicate){
+        boolean b = predicate.test(name);
+        System.out.println("字符串的是否超过10："+b);
+    }
+
+    public static void main(String[] args) {
+        method("aaswedsdw2sss",t->t.length()>10);
+    }
+}
---
+++ FunctionalInterface/src/com/pupu/demo07/Function/Demo02FunctionAndThe.java
@@ -0,0 +1,19 @@
+package com.pupu.demo07.Function;
+
+import java.util.function.Function;
+
+/**
+ * @author : lipu
+ * @since : 2020-08-05 06:46
+ */
+public class Demo02FunctionAndThe {
+
+    private static void method(String name, Function<String,Integer> function1,Function<Integer,Integer> function2){
+        Integer num = function1.andThen(function2).apply(name);
+        System.out.println(num+10);
+    }
+
+    public static void main(String[] args) {
+        method("10",t->Integer.valueOf(t),t->t-2);
+    }
+}
