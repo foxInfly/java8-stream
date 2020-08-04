634c220f52fefe22ae568a32b9c24aa920401ee4
puli <123@123.com>
2020-08-04 22:23:45 +0800

    demo-02 提交

--- FunctionalInterface/src/com/pupu/demo05/Consumer/Demo01Consumer.java
+++ FunctionalInterface/src/com/pupu/demo05/Consumer/Demo01Consumer.java
@@ -1,8 +1,18 @@
 package com.pupu.demo05.Consumer;
 
+import java.util.function.Consumer;
+
 /**
  * @author : lipu
  * @since : 2020-08-04 22:15
  */
 public class Demo01Consumer {
+
+    private static void consumerString(Consumer<String> consumer){
+        consumer.accept("Hello");
+    }
+
+    public static void main(String[] args) {
+        consumerString(s-> System.out.println(s.toUpperCase()));
+    }
 }
