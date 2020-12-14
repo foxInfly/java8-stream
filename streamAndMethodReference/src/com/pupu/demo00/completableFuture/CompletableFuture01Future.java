40d9eb4ce83f3273e97efab08e7bd15b62c7faa6
lipu <lipu@zgzhongnan.com>
2020-12-14 11:48:41 +0800

    CompletableFuture

--- streamAndMethodReference/src/com/pupu/demo00/completableFuture/CompletableFuture01Future.java
+++ streamAndMethodReference/src/com/pupu/demo00/completableFuture/CompletableFuture01Future.java
@@ -1,26 +1,33 @@
 package com.pupu.demo00.completableFuture;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.*;
 
 /**
  *
  */
-public class Demo01Future {
+public class CompletableFuture01Future {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        //定义一个异步任务
-        Future<String> future = executor.submit(() -> {
-            Thread.sleep(2000);
-            return "hello world";
-        });
+
         //轮询获取结果
-        while(true){
-            if (future.isDone()) {
-                System.out.println(future.get());
-                break;
+        for (int i = 0; i < 5; i++) {
+            //定义一个异步任务
+            Future<String> future = executor.submit(() -> {
+                Thread.sleep(2000);
+                return "hello world: " + new SimpleDateFormat("yyyy/MM/dd hh:MM:ss").format(new Date());
+            });
+
+            while (true) {
+                if (future.isDone()) {
+                    System.out.println(future.get());
+                    break;
+                }
             }
+
         }
     }
 
@@ -31,6 +38,7 @@ public class Demo01Future {
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             System.out.println("run end ...");
         });
@@ -44,12 +52,13 @@ public class Demo01Future {
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             System.out.println("run end ...");
             return System.currentTimeMillis();
         });
 
         long time = future.get();
-        System.out.println("time = "+time);
+        System.out.println("time = " + time);
     }
 }
