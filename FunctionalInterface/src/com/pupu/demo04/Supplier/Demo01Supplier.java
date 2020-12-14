98da7f6617349d1d8d5c8d965cc571c4260a042d
lipu <lipu@zgzhongnan.com>
2020-12-14 10:16:39 +0800

    supplier，生产型，返回指定类型的值

--- FunctionalInterface/src/com/pupu/demo04/Supplier/Demo01Supplier.java
+++ FunctionalInterface/src/com/pupu/demo04/Supplier/Demo01Supplier.java
@@ -3,12 +3,16 @@ package com.pupu.demo04.Supplier;
 import java.util.function.Supplier;
 
 /**
+ * Supplier供应者，根据给定的入参生成指定类型的返回值
+ *
  * @author : lipu
  * @since : 2020-08-04 21:58
  */
 public class Demo01Supplier {
 
-    private static String getString(Supplier<String> supplier){
+    private static String getString(Supplier<String> supplier) {
+        String s = supplier.get();
+        System.out.println(s);
         return supplier.get();
     }
 
@@ -16,6 +20,6 @@ public class Demo01Supplier {
         String msg1 = "Hello ";
         String msg2 = "World";
 
-        System.out.println(getString(()->msg1+msg2));
+        System.out.println(getString(() -> msg1 + msg2));
     }
 }
