a57312e9bd8851120292f80d956c3cd3e6095f1b
lipu <lipu@zgzhongnan.com>
2020-12-14 09:53:25 +0800

    lambda延迟加载

--- FunctionalInterface/src/com/pupu/demo02/lambda/LoggerDelayDemo03.java
+++ FunctionalInterface/src/com/pupu/demo02/lambda/LoggerDelayDemo03.java
@@ -1,14 +1,15 @@
 package com.pupu.demo02.lambda;
 
 /**
- * 使用Lambda优化日志案例：延迟加载
+ * 使用Lambda优化日志案例：延迟加载,瓶装代码是在show()方法中执行的，如果级别不够，不会执行，就延迟加载了
  *
  * @author : lipu
  * @since : 2020-08-04 21:33
  */
 public class LoggerDelayDemo03 {
 
-    public static void showLog(int level, MessageBuilder message){
+    public static void showLog(int level, MessageBuilder message) {
+        System.out.println("MessageBuilder:"+message.builderMessage());
         //对日志级别进行判断，如果是1级别，name输出日志信息
         if (level == 1) {
             System.out.println(message.builderMessage());
@@ -21,7 +22,9 @@ public class LoggerDelayDemo03 {
         String msg2 = "World ";
         String msg3 = "Java";
 
-        showLog(2,()->{
-            System.out.println("Lambda执行");return msg1+msg2+msg3;});
+        showLog(2, () -> {
+            System.out.println("Lambda执行");
+            return msg1 + msg2 + msg3;
+        });
     }
 }
--- FunctionalInterface/src/com/pupu/demo02/lambda/LoggerDemo01.java
+++ FunctionalInterface/src/com/pupu/demo02/lambda/LoggerDemo01.java
@@ -2,7 +2,7 @@ package com.pupu.demo02.lambda;
 
 /**
  * 日志案例
- *      以下代码存在性能浪费
+ *      以下代码存在性能浪费,msg1+msg2+msg3不管执不执行，都会走字符串拼装
  * @author : lipu
  * @since : 2020-08-04 20:37
  */
@@ -10,6 +10,7 @@ public class LoggerDemo01 {
 
     public static void showLog(int level, String message){
         //对日志级别进行判断，如果是1级别，name输出日志信息
+        System.out.println(message);
         if (level == 1) {
             System.out.println(message);
         }
@@ -21,6 +22,6 @@ public class LoggerDemo01 {
         String msg2 = "World";
         String msg3 = "Java";
 
-        showLog(1,msg1+msg2+msg3);
+        showLog(2,msg1+msg2+msg3);
     }
 }
